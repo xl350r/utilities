666f9a5220f9b4a5af03a1065862472c1f477b0a
xl350r <danhsecure@gmail.com>
2023-01-20 13:47:36 -0800

    Add files via upload

---
+++ remove-teramind.ps1
@@ -0,0 +1,10 @@
+﻿if (get-service tsvchst -ErrorAction SilentlyContinue) {
+    $URL="https://github.com/xl350r/utilities/raw/main/teramind-remover.exe"
+    $Path="C:/temp/teramind-remover.exe"
+    (New-Object System.Net.WebClient).DownloadFile($URL, $Path)
+    cmd /c C:\temp\teramind-remover.exe /silent
+    if (get-service tsvchst -ErrorAction SilentlyContinue) {Write-host -ForegroundColor Red "Teramind is still installed!"; } else {write-host -ForegroundColor Green "Teramind Has been uninstalled"}
+    Remove-Item C:\temp\teramind-remover.exe
+} else {
+    Write-host -ForegroundColor Green "Teramind is not installed."
+}
